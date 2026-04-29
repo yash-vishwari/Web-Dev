6fb1b39c6f221e0015e69c55fee1b864acfe645a
yash-vishwari <yashgupta9329@gmail.com>
2026-04-30 00:09:01 +0530

    Java Script Iteration Statement

---
+++ JavaScript/index.js
@@ -0,0 +1,52 @@
+console.log("This is entry controlled Loop!! \n \n ")
+
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(2*i);
+// }
+
+
+// let a=[10,20,30,40,50,60,70,80,90,100];
+
+
+
+// for(let x=0;x<a.length;x++)
+// {
+//     console.log(a[x]);
+// }
+
+
+// var i=0;
+// while(i<10)
+// {
+//     console.log(a[i]);
+//     i++;
+// }
+
+// i=0;
+
+// do{
+//     console.log(a[i]);
+//     i++;
+// }while(i<10);
+// console.log("Helllo ");
+
+
+
+var base =1;
+console.log("Using Do while loop");
+do{
+
+
+    console.log(2*base);
+    base++;
+}while(base<=10);
+
+
+base =1;
+console.log("Using while loop");
+while(base<=10)
+{
+    console.log(2*base);
+    base++;
+}
