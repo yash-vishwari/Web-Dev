9bbf1a7b83c9f27eeb968fbae5c5ec0e3cfe3354
yash-vishwari <yashgupta9329@gmail.com>
2026-04-30 23:55:44 +0530

    Javascript Conditional Statements

---
+++ JavaScript/ConditionalStatements/script.js
@@ -0,0 +1,135 @@
+
+let age =5;
+
+if(age >=18)
+{
+    console.log("Eligible for voting");
+}
+
+
+else{
+
+    console.log(" Not eligible for Voting");
+}
+
+
+let num =4;
+if(num %2 ==0)
+{
+    console.log(` Number ${num} is even`);
+}
+
+else{
+     console.log(`Number ${num} is odd`);
+}
+
+
+
+let grade =93;
+
+if(grade>90)
+{
+    console.log("Grade A");
+}
+
+
+else if(grade >75 && grade <=90)
+{
+    console.log("Grade B");
+}
+
+if(grade >50 && grade<=75)
+{
+    console.log("Grade C");
+}
+
+else{
+    console.log("Fail");
+}
+
+let username ="calmBoy";
+let password ="NGU"
+
+let user ="calmBoy";
+let pswd ="NGU";
+if(username ===user && password ===pswd )
+{
+    console.log("Login Successfull");
+}
+else{
+    console.log("Login Failed");
+}
+
+
+
+
+let name ="Pihu";
+let data;
+
+// if(name). //id string pr object is not empty means length >0 -----> treated as True
+// {
+//     data =name;
+// }
+
+// else{
+//     data ="N/A";
+// }
+//  --------------------------------ShortHand-------------------------------------
+
+
+data =name || "N/A";
+
+
+console.log(data);
+// public long countOne(String num,int ind,int tight)
+// {
+
+//     if(ind ==num.length())
+//     {
+//         return 1;
+//     }
+
+
+//     int lb =0;
+//     int hb =9;
+//     if(tight ==1)
+//     {
+//         hb =num.charAt(ind)-'0';
+//     }
+
+
+//     for()
+// // }
+
+
+
+
+let choice =30;
+
+switch(choice)
+{
+
+    
+    case 1:{
+        console.log("tea");
+        break;
+    }
+
+    case 2:{
+        console.log("Lassi");
+        break;
+    }
+
+    
+    case 3:{
+        console.log("Coffee");
+        break;
+    }
+
+    default: {
+        console.log("paani");
+    }
+    
+
+}
+
---
+++ JavaScript/ForLoop_Practice/index.js
@@ -0,0 +1,166 @@
+console.log("This is entry controlled Loop!! \n \n ")
+
+
+console.log("Task 1 Print numbers from 1 to 5 using a for loop");
+for(let i=1;i<=5;i++)
+{
+    console.log(i);
+}
+
+console.log("Task 2 Print numbers from 1 to 10 using a for loop");
+for(let i=1;i<=10;i++)
+{
+    console.log(i);
+}
+
+console.log("Task 3 Print numbers from 10 to 1 (reverse counting)");
+
+for(let i=10;i>=1;i--)
+{
+    
+     console.log(i);
+    
+    
+}
+
+console.log("Task 4 Print even numbers from 2 to 10");
+
+for(let i=1;i<=10;i++)
+{
+    if(i%2 ==0)
+    {
+        console.log(i);
+    }
+    
+}
+console.log("Task 5 Print odd numbers from 1 to 9");
+for(let i=1;i<=9;i++)
+{
+    if(i%2 !=0)
+    {
+        console.log(i);
+    }
+    
+}
+
+console.log("Task 6 Print the table of 15 up to 10 steps");
+for(let i=1;i<=10;i++)
+{
+     console.log(15*i);
+    
+    
+}
+
+console.log("Task 7 Print numbers from 1 to 20 with increment of 2");
+
+
+
+for(let i=1;i<=20;i=i+2)
+{
+    
+        console.log(i);
+    
+    
+}
+
+console.log("Task 8 Print numbers from 5 to 50 with increment of 5");
+
+for(let i=5;i<=50;i=i+5)
+{
+    
+        console.log(i);
+    
+    
+}
+
+console.log("Task 9 Create a loop and print the square of numbers from 1 to 10");
+
+for(let i=1;i<=10;i++)
+{
+    
+     console.log(i*i);
+    
+    
+}
+
+console.log("Task 10 Print numbers from 1 to 10 that are divisible by 3");
+for(let i=1;i<=10;i++)
+{
+    if(i%3 ==0)
+    {
+        console.log(i);
+    }
+    
+}
+
+
+
+
+
+console.log("------------------------------------While Loop Assignment");
+
+
+
+console.log("Write a program to print numbers from 1 to 5 using a while loop.")
+
+
+let i1=0;
+while(i1<=5)
+{
+    console.log(i);
+    i++;
+}
+
+console.log("Write a program to print numbers from 5 to 1 using a while loop.")
+let i2 =5;
+
+while(i2>=0)
+{
+    console.log(i);
+    i--;
+}
+
+// let a=[10,20,30,40,50,60,70,80,90,100];
+
+
+
+// for(let x=0;x<a.length;x++)
+// {
+//     console.log(a[x]);
+// }
+
+
+// var i=0;
+// while(i<10)
+// {
+//     console.log(a[i]);
+//     i++;
+// }
+
+// i=0;
+
+// do{
+//     console.log(a[i]);
+//     i++;
+// }while(i<10);
+// console.log("Helllo ");
+
+
+
+// var base =1;
+// console.log("Using Do while loop");
+// do{
+
+
+//     console.log(2*base);
+//     base++;
+// }while(base<=10);
+
+
+// base =1;
+// console.log("Using while loop");
+// while(base<=10)
+// {
+//     console.log(2*base);
+//     base++;
+// }
