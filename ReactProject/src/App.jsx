a157a8d1befed7cb281c2879f4bdd0f8c95a1d6c
yash-vishwari <yashgupta9329@gmail.com>
2026-06-04 23:39:02 +0530

    React Project

--- ReactProject/src/App.jsx
+++ ReactProject/src/App.jsx
@@ -1,20 +1,28 @@
-import Header from "./components/Header";
-
+import Header from './components/Header.jsx';
+import Home from'./pages/Home.jsx'
+import About from "./pages/About.jsx";
+import Product from "./pages/Product.jsx";
+import ContactUs from "./pages/ContactUs.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import "./App.css"
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Header />
-
-      <div id="abcd">My first react app</div>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-        nostrum provident error dolorem laborum illum perspiciatis dignissimos
-        cupiditate, nisi quod expedita, modi molestias. Reiciendis voluptatibus
-        quod beatae animi odio ut.
-      </p>
-
-      <button>click Here</button>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/contactus" element={<ContactUs />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+         
+        </Routes>
+      </BrowserRouter>
+      {}
     </>
   );
 }
