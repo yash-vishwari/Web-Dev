34680b8ab19256f75eb70e4ada39cdb95c5d6519
yash-vishwari <yashgupta9329@gmail.com>
2026-06-09 22:53:08 +0530

    React Project Login Page

--- ReactProject/src/pages/Login.jsx
+++ ReactProject/src/pages/Login.jsx
@@ -3,32 +3,50 @@ import { useState } from "react";
 function Login() {
   const [userName, setuserName] = useState("");
   const [password, setpassword] = useState("");
-
+  function submitFunc(e)
+  {
+    e.preventDefault();
+    console.log(userName, password);
+  }
   return (
     <>
-      <form action="">
-        <div>
-          <label htmlFor="UserName">Username :</label>
-          <input
-            type="text"
-            id="UserName"
-            value={userName}
-            onChange={(e) => {
-              setuserName(e.target.value);
-            }}
-          />
-        </div>
+      <form
+        action=""
+        onSubmit={submitFunc}
+        className="form-control d-flex  vh-100 justify-content-center bg-info-subtle"
+      >
+        <div className="d-flex flex-column gap-3 align-items-center mt-5">
+          <p className="fs-1 fw-bold text-primary">Login Page</p>
+          <div>
+            <label htmlFor="UserName">Username :</label>
+            <input
+              type="text"
+              id="UserName"
+              value={userName}
+              class="form-control"
+              onChange={(e) => {
+                setuserName(e.target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="PassWord">Password :</label>
+            <input
+              type="text"
+              id="PassWord"
+              value={password}
+              class="form-control"
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+          </div>
 
-        <div>
-          <label htmlFor="PassWord">Password :</label>
-          <input
-            type="text"
-            id="PassWord"
-            value={password}
-            onChange={(e) => {
-              setuserName(e.target.value);
-            }}
-          />
+          <div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
         </div>
       </form>
     </>
