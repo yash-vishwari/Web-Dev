e9422dbb3037702145ec84c32e13761efe42cd77
yash-vishwari <yashgupta9329@gmail.com>
2026-06-10 23:58:21 +0530

    React Project

--- ReactProject/src/main.jsx
+++ ReactProject/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Header from './components/Header.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
+   
     <App />
+    
+    
   </StrictMode>,
-)
+);
