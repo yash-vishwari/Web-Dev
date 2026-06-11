58bfdf3456ff88b904bbba2108b6984fec5997c2
yash-vishwari <yashgupta9329@gmail.com>
2026-06-11 23:27:09 +0530

    Fake Store React Project

---
+++ Fake_Store/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
+import Product from "./pages/Product.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
