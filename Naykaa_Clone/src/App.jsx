c967c8840c9e32c03ca2fd46a770c36414e37815
yash-vishwari <yashgupta9329@gmail.com>
2026-06-18 22:50:28 +0530

    App.jsx File Code Updated

--- Naykaa_Clone/src/App.jsx
+++ Naykaa_Clone/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
-          
+            
         </div>
         <button
           type="button"
