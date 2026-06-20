00fe564488b8782028e63cdd18dd993ae5ddba22
yash-vishwari <yashgupta9329@gmail.com>
2026-06-20 21:35:29 +0530

    Updated Count Button

--- Naykaa_Clone/src/App.jsx
+++ Naykaa_Clone/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
         <div>
           <h1>Get started</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to Test <code>HMR</code>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
             
         </div>
@@ -27,7 +27,7 @@ function App() {
           className="counter"
           onClick={() => setCount((count) => count + 1)}
         >
-          Count is {count}
+          Count is : {count}
         </button>
       </section>
 
